f2dac486aab3144ceb9b31f69efa659ad29b821b
SEvanGF <evan.gonzalez2901@gmail.com>
2024-02-09 15:23:12 -0500

    Changed All Code to be for All sensors in one graph

--- Scripts/Looking at Raw Data.R
+++ Scripts/Looking at Raw Data.R
@@ -1,13 +1,108 @@
+#This script is not that important, it is here just to look at the temperature data and see why 
+#certain smooth functions seem to deviate significantly from the others
 
- Falaise_subset <- subset(edited_temp_data, Park == "Falaise" | Park == "Falaise_Reference") 
+## Falaise
+
+ Falaise_all <- subset(falaise_subset, DayTime == "Day")
  
- Falaise_1 <- subset(falaise_subset, Sensor_Name == "Falaise_1")
- 
- Falaise_1_summary <- Falaise_1 %>%
-   group_by(Date, DayTime) %>%
+ Falaise_all_summary <- Falaise_all %>%
+   group_by(Date, Sensor_Name) %>%
    summarize(meanT = mean(Temperature))
  
- Falaise_1_summary %>%
+ Falaise_all_summary %>%
    ggplot()+
+   geom_line(aes(x=Date, y=meanT))+
+   geom_smooth(method="loess", span=.6, se=F, linewdith=2.5, aes(x=Date, y=meanT))+
+   theme_classic()+
+   ggtitle("Falaise_ALL")+
+   facet_wrap(~Sensor_Name)
+ 
+ # Falaise_subset <- subset(edited_temp_data, Park == "Falaise" | Park == "Falaise_Reference") 
+ # 
+ # Falaise_1 <- subset(falaise_subset, Sensor_Name == "Falaise_1")
+ # 
+ # Falaise_1_summary <- Falaise_1 %>%
+ #   group_by(Date, DayTime) %>%
+ #   summarize(meanT = mean(Temperature))
+ # 
+ # Falaise_1_summary %>%
+ #   ggplot()+
+ #   geom_line(aes(x=Date , y= meanT))+
+ #   geom_smooth(method = "loess", span=.6, se=F, linewidth=2.5, aes(x = Date, y = meanT,)) +
+ #   theme_classic()+
+ #   ggtitle("Falaise_1")+
+ #   facet_wrap(~DayTime)
+ 
+
+## CdesP
+ 
+ CdesP_all <- subset(CdesP_subset, DayTime == "Day")
+ 
+ CdesP_all_summary <- CdesP_all %>%
+   group_by(Date, Sensor_Name) %>%
+   summarize(meanT = mean(Temperature))
+ 
+ CdesP_all_summary %>%
+   ggplot()+
+   geom_line(aes(x=Date, y=meanT))+
+   geom_smooth(method="loess", span=.6, se=F, linewidth=2.5, aes(x=Date, y=meanT))+
+   theme_classic()+
+   ggtitle("CdesP_ALL")+
+   facet_wrap(~Sensor_Name)
+ 
+ 
+# CdesP_subset <- subset(edited_temp_data, Park == "CdesP" | Park == "CdesP_Reference") 
+#  
+#  CdesP_2 <- subset(CdesP_subset, Sensor_Name == "CdesP_2")
+#  
+#  CdesP_2_summary <- CdesP_2 %>%
+#    group_by(Date, DayTime) %>%
+#    summarize(meanT = mean(Temperature))
+#  
+#  CdesP_2_summary %>%
+#    ggplot()+
+#    geom_smooth(method = "loess", span=.7, se=F, linewidth=2.5, aes(x = Date, y = meanT,)) +
+#     geom_line(aes(x=Date , y= meanT))+
+#    theme_classic()+
+#    ggtitle("CdesP_2")
+#    facet_wrap(~DayTime)
+ 
+### MHM
+ 
+ MHM_subset <- subset(edited_temp_data, Park == "MHM" | Park == "MHM_Reference") 
+ 
+ MHM_all <- subset(MHM_subset, DayTime == "Day")
+ 
+ MHM_all_summary <- MHM_4 %>%
+   group_by(Date, Sensor_Name) %>%
+   summarize(meanT = mean(Temperature))
+ 
+ MHM_all_summary %>%
+   ggplot()+
+   geom_smooth(method = "loess", span=.6, se=F, linewidth=2.5, aes(x = Date, y = meanT,)) +
    geom_line(aes(x=Date , y= meanT))+
-   theme_classic()
+   theme_classic()+
+   ggtitle("MHM_4")+
+   facet_wrap(~Sensor_Name)
+ 
+ ### TP
+ 
+ TP_subset <- subset(edited_temp_data, Park == "TP" | Park == "TP_Reference") 
+ 
+ TP_all <- subset(TP_subset, DayTime == "Day")
+ 
+ TP_all_summary <- TP_all %>%
+   group_by(Date, Sensor_Name) %>%
+   summarize(meanT = mean(Temperature))
+ 
+ TP_all_summary %>%
+   ggplot()+
+   geom_line(aes(x=Date, y=meanT))+
+   geom_smooth(method="loess", span=.6, se=F, linewidth=2.5, aes(x=Date, y=meanT))+
+   theme_classic()+
+   ggtitle("TP_ALL")+
+   facet_wrap(~Sensor_Name)
+ 
+
+  
+ 
